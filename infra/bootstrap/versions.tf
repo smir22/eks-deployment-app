74cefcc923e1f926cf2270c287b7810e35694137
smirvf <saahirmirvf@gmail.com>
2026-08-07 17:28:56 +0100

    feat(bootstrap): move state to the s3 backend with native locking (#3)

--- infra/bootstrap/versions.tf
+++ infra/bootstrap/versions.tf
@@ -7,4 +7,12 @@ terraform {
       version = "~> 6.0"
     }
   }
+  backend "s3" {
+    bucket       = "eks-deployment-state-s3-sm22"
+    key          = "bootstrap/terraform.tfstate"
+    region       = "eu-west-2"
+    use_lockfile = true
+    encrypt      = true
+    kms_key_id   = "arn:aws:kms:eu-west-2:112639119087:key/41c68d5f-3a3e-4463-b022-8b1e66008f6a"
+  }
 }
